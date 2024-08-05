0764d9ac325b413812e03ad9ff2fb26773a518e0
= <gazdarshariq0@gmail.com>
2024-08-05 14:28:53 +0500

    Card thing add!

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { NewsCard } from "./components/Card";
 function CardContainer() {
-  return <div></div>;
+  let baseUrl = `https://newsapi.org/v2/top-headlines?country=pk&apiKey=0a4ece348d504c7c9f16781fb19c222b
+`;
+
+  return;
+  <div></div>;
 }
 
 export default CardContainer;
