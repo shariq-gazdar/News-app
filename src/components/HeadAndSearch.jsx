0b65c11b7588908fc4eb44b0e688a36c646bb7f6
= <gazdarshariq0@gmail.com>
2024-08-12 02:15:04 +0500

    categories add!

--- src/components/HeadAndSearch.jsx
+++ src/components/HeadAndSearch.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { TextInput } from "flowbite-react";
+import { Button, TextInput } from "flowbite-react";
 import searchIcon from "./media/Svgs/search_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
 
 function HeadAndSearch({ onSearch }) {
@@ -18,17 +18,23 @@ function HeadAndSearch({ onSearch }) {
     <div className="HeadAndSearch flex flex-col items-center py-10">
       <h1 className="text-3xl font-bold dark:text-white">Get Daily News</h1>
       <div className="max-w-md my-5 flex relative">
-        <TextInput
-          id="email4"
-          type="search"
-          placeholder="Search News From Topics"
-          required
-          className="w-96"
-          onChange={handleSearchChange}
-        />
-        <button className="absolute left-80 top-2 " onClick={handleSearchClick}>
-          <img src={searchIcon} alt="Search" />
-        </button>
+        <Button id="regional" />
+        <Button id="technology" />
+        <Button id="lifestyle" />
+        <Button id="business" />
+        <Button id="general" />
+        <Button id="programming" />
+        <Button id="science" />
+        <Button id="entertainment" />
+        <Button id="world" />
+        <Button id="sports" />
+        <Button id="finance" />
+        <Button id="academia" />
+        <Button id="politics" />
+        <Button id="health" />
+        <Button id="opinion" />
+        <Button id="food" />
+        <Button id="game" />
       </div>
     </div>
   );
